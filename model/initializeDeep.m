a0e1e025a9ddf65ceb9ecaac7289cae821ed7423
TheKipmaster <fkipman@hotmail.com>
2018-05-17 19:43:20 -0300

    Modifies initialization function to return cell array instead of huge vector

--- model/initializeDeep.m
+++ model/initializeDeep.m
@@ -1,9 +1,10 @@
 function params = initializeDeep(ThetaDims)
   L = length(ThetaDims);
-  params = [];
+  params = {};
   for l=2:L
     wl = randInitializeWeights(ThetaDims(l-1), ThetaDims(l));
-    bl = zeros(ThetaDims(l-1), 1);
-    params = [params; wl(:); bl(:)];
+    bl = zeros(1, ThetaDims(l));
+    params{l-1}{1} = wl;
+    params{l-1}{2} = bl;
   end
 end
