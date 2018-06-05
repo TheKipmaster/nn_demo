62540673d5a24d95c377689888cd6484f5737d56
TheKipmaster <fkipman@hotmail.com>
2018-06-05 15:19:23 -0300

    Implements L2 regularization

--- model/backwardActivation.m
+++ model/backwardActivation.m
@@ -1,5 +1,5 @@
 % computes partial derivatives of w, b, and A with respect to the cost
-function [dA_prev, dw, db] = backwardActivation(dA, cache, non_liearity)
+function [dA_prev, dw, db] = backwardActivation(dA, cache, lambda, non_liearity)
   [w, b, A_prev, z] = cache{:};
   m = size(A_prev, 1);
 
@@ -10,7 +10,7 @@ function [dA_prev, dw, db] = backwardActivation(dA, cache, non_liearity)
     dz = dA.*reluPrime(z);
   end
 
-  dw = (dz'*A_prev)/m;
+  dw = (dz'*A_prev)/m + (lambda/m)*w;
   db = sum(dz, 1)/m;
   dA_prev = dz*w;
 end
--- model/computeCost.m
+++ model/computeCost.m
@@ -1,5 +1,12 @@
 % Compute cost based on the final output of the network
-function J = computeCost(AL, Y, lambda)
+function J = computeCost(AL, Y, params, lambda)
   m = size(Y, 1);
-  J = (Y'*log(AL)+(1-Y')*(log(1-AL)))/-m;
+  L = length(params);
+  reg = 0;
+
+  for l=1:L
+    reg += sum(sum(params{l}{1}.^2));
+  end
+  unregJ = (Y'*log(AL)+(1-Y')*(log(1-AL)))/-m;
+  J = unregJ + (lambda/2*m)*reg;
 end
