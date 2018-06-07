36c1bfdd9b53fb1fed1aa15f2dd6211de76663f2
TheKipmaster <fkipman@hotmail.com>
2018-06-07 12:56:18 -0300

    Refactors demo function

--- model/demo.m
+++ model/demo.m
@@ -27,21 +27,22 @@ X = (X-128)/255; % normalize inputs
 sel = randperm(size(X, 1));
 sel = sel(1:100);
 
-X_train = X(1:409, :);
-X_valid = X(410:511, :);
-
-Y_train = Y(1:409, :);
-Y_valid = Y(410:511, :);
-
 displayData(X(sel, :));
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
+% Divide dataset into training and validation subsets
+X_train = X(1:409, :);
+Y_train = Y(1:409, :);
+
+X_valid = X(410:511, :);
+Y_valid = Y(410:511, :);
+
 train_accuracy = [];
 valid_accuracy = [];
+% Training loop
 for i=1:rounds
-  % Training loop
   params = initializeDeep(dimensions);      % Initialize weight matrices with values between 0 and 1
   [params, costs] = training(params, num_iter, X_train, Y_train, learning_rate, lambda);
   train_predictions = predict(X_train, params);
@@ -50,7 +51,10 @@ for i=1:rounds
   train_accuracy = [train_accuracy; accuracy(train_predictions, Y_train)];
   valid_accuracy = [valid_accuracy; accuracy(valid_predictions, Y_valid)];
 end
-[train_accuracy, valid_accuracy; sum(train_accuracy)/10, sum(valid_accuracy)/10]
+fprintf('    train  |  valid\n')
+display([train_accuracy, valid_accuracy]);
+fprintf('         means      \n')
+display([sum(train_accuracy)/rounds, sum(valid_accuracy)/rounds])
 
 %  Randomly permute examples
 m = size(X_valid, 1);
@@ -58,18 +62,23 @@ rp = randperm(m);
 
 %  Display random example and corresponding (ground truth) label as well as prediction
 for i = 1:m
-    % Display
+    % Display example
     fprintf('\nDisplaying Example Image\n');
     displayData(X_valid(rp(i), :));
 
+    % Get prediction and true label for chosen example
     pred = valid_predictions(rp(i),:);
-    % translate binary output of network to concrete label
-    if pred == 1
-      answer = 'plane';
-    else
-      answer = 'not plane';
-    end
-    fprintf('\nNeural Network Prediction: %s \n', answer);
+    true = Y_valid(rp(i));
+
+    % Convert binary prediction and true label into descriptive strings
+    pair = [pred, true] + 1;
+    mask = {'not plane', 'plane'};
+    aux = mask(pair);
+    answer = aux{1};
+    truth = aux{2};
+
+    % Display legend
+    fprintf('\nNeural Network Prediction: %s (was actually %s) \n', answer, truth);
 
     % Pause with quit option
     s = input('Paused - press enter to continue, q to exit:','s');
